28a7995bf502163aa90b1e44715ab9f08f1110e3
syuu1228 <syuu@dokukino.com>
2010-04-02 14:45:28 +0900

    argorithm changed

--- src/ow/tool/dhtshell/Main.java
+++ src/ow/tool/dhtshell/Main.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package ow.tool.dhtshell;
 
 import java.io.IOException;
@@ -62,190 +61,191 @@ import ow.tool.util.toolframework.AbstractDHTBasedTool;
  * This shell is an utility to use/test a DHT.
  */
 public final class Main extends AbstractDHTBasedTool<String>
-		implements Interruptible {
-	private final static String COMMAND = "owdhtshell";	// A shell/batch script provided as bin/owdhtshell
-	private final static int SHELL_PORT = -1;
-	public final static int XMLRPC_PORT_DIFF = +1;
-	private final static int XMLRPC_PORT_RANGE = 100;
+        implements Interruptible {
 
-	public final static String ENCODING = "UTF-8";
+    private final static String COMMAND = "owdhtshell";	// A shell/batch script provided as bin/owdhtshell
+    private final static int SHELL_PORT = -1;
+    public final static int XMLRPC_PORT_DIFF = +1;
+    private final static int XMLRPC_PORT_RANGE = 100;
+    public final static String ENCODING = "UTF-8";
+    private final static Class/*Command<<DHT<String>>>*/[] COMMANDS = {
+        StatusCommand.class,
+        InitCommand.class,
+        GetCommand.class, PutCommand.class, RemoveCommand.class,
+        SetTTLCommand.class, SetSecretCommand.class,
+        LocaldataCommand.class,
+        //		SourceCommand.class,
+        HelpCommand.class,
+        QuitCommand.class,
+        HaltCommand.class,
+        ClearCommand.class,
+        SuspendCommand.class, ResumeCommand.class
+    };
+    private final static List<Command<DHT<String>>> commandList;
+    private final static Map<String, Command<DHT<String>>> commandTable;
 
-	private final static Class/*Command<<DHT<String>>>*/[] COMMANDS = {
-		StatusCommand.class,
-		InitCommand.class,
-		GetCommand.class, PutCommand.class, RemoveCommand.class,
-		SetTTLCommand.class, SetSecretCommand.class,
-		LocaldataCommand.class,
-//		SourceCommand.class,
-		HelpCommand.class,
-		QuitCommand.class,
-		HaltCommand.class,
-		ClearCommand.class,
-		SuspendCommand.class, ResumeCommand.class
-	};
+    static {
+        commandList = ShellServer.createCommandList(COMMANDS);
+        commandTable = ShellServer.createCommandTable(commandList);
+    }
+    private Thread mainThread = null;
 
-	private final static List<Command<DHT<String>>> commandList;
-	private final static Map<String,Command<DHT<String>>> commandTable;
+    protected void usage(String command) {
+        super.usage(command, "[-p <shell port>] [--acl <ACL file>] [-n] [--upnp]");
+    }
 
-	static {
-		commandList = ShellServer.createCommandList(COMMANDS);
-		commandTable = ShellServer.createCommandTable(commandList);
-	}
+    public static void main(String[] args) {
+        SLF4JBridgeHandler.install();
+        (new Main()).start(args);
+    }
 
-	private Thread mainThread = null;
+    protected void start(String[] args) {
+        Shell<DHT<String>> stdioShell = null;
 
-	protected void usage(String command) {
-		super.usage(command, "[-p <shell port>] [--acl <ACL file>] [-n] [--upnp]");
-	}
+        stdioShell = this.init(args, System.in, System.out, true);
 
-	public static void main(String[] args) {
-            SLF4JBridgeHandler.install();
-		(new Main()).start(args);
-	}
+        if (stdioShell != null) {
+            stdioShell.run();	// this call is blocked
+        }
+    }
 
-	protected void start(String[] args) {
-		Shell<DHT<String>> stdioShell = null;
+    /**
+     * Implements {@link EmulatorControllable#invoke(int, String[], PrintStream)
+     * EmulatorControllable#invoke}.
+     */
+    public Writer invoke(String[] args, PrintStream out) {
+        Shell<DHT<String>> stdioShell = this.init(args, null, out, false);
 
-		stdioShell = this.init(args, System.in, System.out, true);
+        if (stdioShell != null) {
+            return stdioShell.getWriter();
+        } else {
+            return null;
+        }
+    }
 
-		if (stdioShell != null) {
-			stdioShell.run();	// this call is blocked
-		}
-	}
+    private Shell<DHT<String>> init(String[] args, InputStream in, PrintStream out, boolean interactive) {
+        int shellPort = SHELL_PORT;
+        AccessController ac = null;
+        boolean disableStdin = false, enableUPnP = false;
 
-	/**
-	 * Implements {@link EmulatorControllable#invoke(int, String[], PrintStream)
-	 * EmulatorControllable#invoke}.
-	 */
-	public Writer invoke(String[] args, PrintStream out) {
-		Shell<DHT<String>> stdioShell = this.init(args, null, out, false);
+        this.mainThread = Thread.currentThread();
 
-		if (stdioShell != null)
-			return stdioShell.getWriter();
-		else
-			return null;
-	}
+        // parse command-line arguments
+        Options opts = this.getInitialOptions();
+        opts.addOption("p", "port", true, "port number");
+        opts.addOption("A", "acl", true, "access control list file");
+        opts.addOption("n", "disablestdin", false, "disable standard input");
+        opts.addOption("u", "upnp", false, "enable UPnP");
 
-	private Shell<DHT<String>> init(String[] args, InputStream in, PrintStream out, boolean interactive) {
-		int shellPort = SHELL_PORT;
-		AccessController ac = null;
-		boolean disableStdin = false, enableUPnP = false;
+        CommandLineParser parser = new PosixParser();
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(opts, args);
+        } catch (ParseException e) {
+            System.out.println("There is an invalid option.");
+            e.printStackTrace();
+            System.exit(1);
+        }
 
-		this.mainThread = Thread.currentThread();
+        parser = null;
+        opts = null;
 
-		// parse command-line arguments
-		Options opts = this.getInitialOptions();
-		opts.addOption("p", "port", true, "port number");
-		opts.addOption("A", "acl", true, "access control list file");
-		opts.addOption("n", "disablestdin", false, "disable standard input");
-                opts.addOption("u", "upnp", false, "enable UPnP");
+        String optVal;
+        optVal = cmd.getOptionValue('p');
+        if (optVal != null) {
+            shellPort = Integer.parseInt(optVal);
+        }
+        optVal = cmd.getOptionValue("A");
+        if (optVal != null) {
+            try {
+                ac = new AccessController(optVal);
+            } catch (IOException e) {
+                System.err.println("An Exception thrown:");
+                e.printStackTrace();
+                return null;
+            }
+        }
+        if (cmd.hasOption('n')) {
+            disableStdin = true;
+        }
+        if (cmd.hasOption('u')) {
+            enableUPnP = true;
+        }
 
-		CommandLineParser parser = new PosixParser();
-		CommandLine cmd = null;
-		try {
-			cmd = parser.parse(opts, args);
-		}
-		catch (ParseException e) {
-			System.out.println("There is an invalid option.");
-			e.printStackTrace();
-			System.exit(1);
-		}
+        // parse remaining arguments
+        // and initialize a DHT
+        DHT<String> dht = null;
+        DHTConfiguration config = DHTFactory.getDefaultConfiguration();
+        /*
+        config.setImplementationName("ChurnTolerantDHT");
+        config.setDirectoryType("PersistentMap");
+        config.setMessagingTransport("UDP");
+        config.setRoutingAlgorithm("Kademlia");
+        config.setRoutingStyle("Iterative");
+        config.setDoExpire(true);
+        config.setDoReputOnRequester(false);
+        config.setUseTimerInsteadOfThread(false);
+         */
+        config.setDoUPnPNATTraversal(enableUPnP);
+        try {
+            dht = super.initialize(DHTFox.APPLICATION_ID, DHTFox.APPLICATION_MAJOR_VERSION,
+                    config,
+                    COMMAND, cmd);
+        } catch (Exception e) {
+            System.err.println("An Exception thrown:");
+            e.printStackTrace();
+            return null;
+        }
 
-		parser = null;
-		opts = null;
+        cmd = null;
 
-		String optVal;
-		optVal = cmd.getOptionValue('p');
-		if (optVal != null) {
-			shellPort = Integer.parseInt(optVal);
-		}
-		optVal = cmd.getOptionValue("A");
-		if (optVal != null) {
-			try {
-				ac = new AccessController(optVal);
-			}
-			catch (IOException e) {
-				System.err.println("An Exception thrown:");
-				e.printStackTrace();
-				return null;
-			}
-		}
-		if (cmd.hasOption('n')) {
-			disableStdin = true;
-		}
-                if (cmd.hasOption('u')) {
-                    enableUPnP = true;
-                }
+        // start a ShellServer
+        ShellServer<DHT<String>> shellServ =
+                new ShellServer<DHT<String>>(commandTable, commandList,
+                new ShowPromptPrinter(), new NoCommandPrinter(), null,
+                dht, shellPort, ac);
+        shellServ.addInterruptible(this);
 
-		// parse remaining arguments
-		// and initialize a DHT
-		DHT<String> dht = null;
-		DHTConfiguration config = DHTFactory.getDefaultConfiguration();
-		config.setImplementationName("ChurnTolerantDHT");
-		config.setDirectoryType("PersistentMap");
-		config.setMessagingTransport("UDP");
-		config.setRoutingAlgorithm("Kademlia");
-		config.setRoutingStyle("Iterative");
-		config.setDoUPnPNATTraversal(enableUPnP);
-		config.setDoExpire(true);
-		config.setDoReputOnRequester(false);
-		config.setUseTimerInsteadOfThread(false);
-		try {
-			dht = super.initialize(DHTFox.APPLICATION_ID, DHTFox.APPLICATION_MAJOR_VERSION,
-					config,
-					COMMAND, cmd);
-		}
-		catch (Exception e) {
-			System.err.println("An Exception thrown:");
-			e.printStackTrace();
-			return null;
-		}
+        Shell<DHT<String>> stdioShell = null;
+        if (disableStdin) {
+            try {
+                Thread.sleep(Long.MAX_VALUE);
+            } catch (InterruptedException e) {
+            }
+        } else {
+            stdioShell = new Shell<DHT<String>>(in, out, shellServ, dht, interactive);
+        }
 
-		cmd = null;
+        return stdioShell;
+    }
 
-		// start a ShellServer
-		ShellServer<DHT<String>> shellServ =
-			new ShellServer<DHT<String>>(commandTable, commandList,
-					new ShowPromptPrinter(), new NoCommandPrinter(), null,
-					dht, shellPort, ac);
-		shellServ.addInterruptible(this);
+    public void interrupt() {
+        if (this.mainThread != null && !this.mainThread.equals(Thread.currentThread())) {
+            this.mainThread.interrupt();
+        }
+    }
 
-		Shell<DHT<String>> stdioShell = null;
-		if (disableStdin) {
-			try {
-				Thread.sleep(Long.MAX_VALUE);
-			} catch (InterruptedException e) {}
-		}
-		else {
-			stdioShell = new Shell<DHT<String>>(in, out, shellServ, dht, interactive);
-		}
+    private static class ShowPromptPrinter implements MessagePrinter {
 
-		return stdioShell;
-	}
+        public void execute(PrintStream out, String hint) {
+            out.print("Ready." + Shell.CRLF);
+            out.flush();
+        }
+    }
 
-	public void interrupt() {
-		if (this.mainThread != null && !this.mainThread.equals(Thread.currentThread()))
-			this.mainThread.interrupt();
-	}
+    private static class NoCommandPrinter implements MessagePrinter {
 
-	private static class ShowPromptPrinter implements MessagePrinter {
-		public void execute(PrintStream out, String hint) {
-			out.print("Ready." + Shell.CRLF);
-			out.flush();
-		}
-	}
+        public void execute(PrintStream out, String hint) {
+            out.print("No such command");
 
-	private static class NoCommandPrinter implements MessagePrinter {
-		public void execute(PrintStream out, String hint) {
-			out.print("No such command");
+            if (hint != null) {
+                out.print(": " + hint);
+            } else {
+                out.print(".");
+            }
+            out.print(Shell.CRLF);
 
-			if (hint != null)
-				out.print(": " + hint);
-			else
-				out.print(".");
-			out.print(Shell.CRLF);
-
-			out.flush();
-		}
-	}
+            out.flush();
+        }
+    }
 }
